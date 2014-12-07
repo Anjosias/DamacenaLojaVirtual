554c0abcd1d9ac6abd6747410c5d6cf933eadaf7
Anjosias <josias1901@hotmail.com>
2014-12-07 20:04:36 -0200

    Primeiro commit

---
+++ Damacena.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Damacena.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestDamacena
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Damacena.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Damacena.LojaVirtual.Web.MvcApplication" Language="C#" %>
